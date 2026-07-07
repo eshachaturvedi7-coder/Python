dff0171b2c3362a03870e239699f3bdc5bbace00
Esha chaturvedi <eshachaturvedi7@gmail.com>
2026-07-07 21:23:51 +0530

    Add files via upload

---
+++ CHAPTER 2/03_operator.py
@@ -0,0 +1,40 @@
+#Arithmetic operators
+
+a=34
+
+b=4
+c=a+b
+print(c)
+
+#Assignment operators
+
+a=4-2 # assign 4-2 in a
+print(a)
+b=6
+
+b+=3 #increment the value of b by 3 and then assign it to b
+print(b)
+
+#Comparison operators
+
+d=5<4
+print(d)
+
+#Logical operators
+e= True and False
+
+#true table of 'or'
+print("true or false is", True or False)
+print("true or true is", True or True)
+print("false or true is", False or True)
+print("false or false is", False or False)
+
+#true table of 'and'
+print("true and false is", True and False)
+print("true and true is", True and True)
+print("false and true is", False and True)
+print("false and false is", False and False)
+
+#not
+
+print(not(False))
---
+++ CHAPTER 2/05_input.py
@@ -0,0 +1,6 @@
+a=int(input("enter number 1: "))  
+b=int(input("enter number 2: "))
+
+print(" number a is :",a)
+print(" number b is :",b)
+print("sum of a and b is :",a+b)
