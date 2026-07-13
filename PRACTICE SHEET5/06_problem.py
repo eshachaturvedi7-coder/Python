fd3796bb735a06d9306de1d0860dfc1722a4fa2d
Esha chaturvedi <eshachaturvedi7@gmail.com>
2026-07-13 21:57:26 +0530

    Add files via upload

---
+++ PRACTICE SHEET5/06_problem.py
@@ -0,0 +1,20 @@
+#Create  an empty dictionary. allow 4 friends to enter their favorite language as value and use key as their names. assume that the names are unique.
+
+d={}
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name : ")
+d.update({name:lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name : ")
+d.update({name:lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name : ")
+d.update({name:lang})
+
+name = input("Enter friends name: ")
+lang = input("Enter Language name : ")
+d.update({name:lang})
+print(d)
