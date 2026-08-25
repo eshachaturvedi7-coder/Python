d8fd5d4f01ba38f5b5935664dedb0e5d303578de
Esha chaturvedi <eshachaturvedi7@gmail.com>
2026-08-25 14:04:29 +0530

    Add files via upload

---
+++ CHAPTER 6/01_conditionals.py
@@ -0,0 +1,8 @@
+a = int(input("Enter your age: "))
+
+if(a>=20):
+    print("your are above the age of consent")
+    print("good for you")
+       
+else:
+    print("you are below the age of consent")
